4e0620c2663df28ba5594db01a870949c5dddc17
Oscar Ruiz <oruizpalma@anzen.com.mx>
2019-01-11 13:12:11 -0600

    Data binding setup added

--- app/src/main/java/com/oscar/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/oscar/example/aboutme/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.oscar.example.aboutme
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
