8c9145a81e9d2b6460af06ea26e7702838098f0a
oruizp <oruizp@live.com>
2019-01-14 17:32:29 -0600

    Data binding added

--- app/src/main/java/com/oscar/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/oscar/example/aboutme/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.oscar.example.aboutme
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.oscar.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button.setOnClickListener { changeText(binding.editText.text.toString()) }
+    }
+
+    private fun changeText(text: String) {
+        binding.textView.text = text
     }
 }
